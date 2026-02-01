2b8d0cf4fc3103178cc29c32753d1534ccc96f8e
shalkovk <kshalkovskiy@gmail.com>
2026-02-01 23:55:23 +0500

    add methods in product service

--- backend/app/services/product_service.py
+++ backend/app/services/product_service.py
@@ -13,6 +13,38 @@ class ProductService:
 
     def get_all_products(self) -> ProductListResponse:
         products = self.product_repository.get_all()
-        product_reponse = [ProductResponse.model_validate(
+        products_reponse = [ProductResponse.model_validate(
             product) for product in products]
-        return ProductListResponse(products=product_reponse, total=len(product_reponse))
+        return ProductListResponse(products=products_reponse, total=len(products_reponse))
+
+    def product_by_id(self, product_id: int) -> ProductResponse:
+        product = self.product_repository.get_by_id(product_id)
+        if not product:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {product_id} not found"
+            )
+        return ProductResponse.model_validate(product)
+
+    def product_by_category(self, category_id: int) -> ProductListResponse:
+        category = self.category_repository.get_by_id(category_id)
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Category with id {category_id} not found"
+            )
+
+        products = self.product_repository.get_by_category(category_id)
+        products_response = [ProductResponse.model_validate(
+            product) for product in products]
+        return ProductResponse(products=products_response, total=len(products_response))
+
+    def create_product(self, product_data: ProductCreate) -> ProductResponse:
+        category = self.category_repository.get_by_id(product_data.category_id)
+        if not category:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Category with id {product_data.category_id}"
+            )
+        product = self.product_repository.create_product(product_data)
+        return ProductResponse.model_validate(product)
