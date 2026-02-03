08a035563ba8016ffd7b069bec2e7a0a25c9c3fb
shalkovk <kshalkovskiy@gmail.com>
2026-02-03 23:06:15 +0500

    add cart service

--- backend/app/services/cart_service.py
+++ backend/app/services/cart_service.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
-from typing import List
+from typing import Dict
 from ..repositories.product_repository import ProductRepository
 from ..repositories.category_repository import CategoryRepository
-from ..schemas.product import ProductListResponse, ProductCreate, ProductResponse
+from ..schemas.cart import CartItem, CartReponse, CartItemCreate, CartItemUpdate
 from fastapi import HTTPException, status
 
 
@@ -10,3 +10,36 @@ class CartSerice:
     def __init__(self, db: Session):
         self.product_repository = ProductRepository(db)
         self.category_repository = CategoryRepository(db)
+
+    def add_to_cart(self, cart_data: Dict[int, int], item: CartItemCreate) -> Dict[int, int]:
+        product = self.product_repository.get_by_id(item.product_id)
+        if not product:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {item.product_id} not found"
+            )
+        if item.product_id in cart_data:
+            cart_data[item.product_id] += item.quantity
+        else:
+            cart_data[item.product_id] = item.quantity
+
+        return cart_data
+
+    def update_cart_item(self, cart_data: Dict[int, int], item: CartItemUpdate) -> Dict[int, int]:
+        if item.product_id not in cart_data:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {item.product_id} not found in cart"
+            )
+
+        cart_data[item.product_id] = item.quantity
+        return cart_data
+
+    def remove_from_cart(self, cart_data: Dict[int, int], product_id: int) -> Dict[int, int]:
+        if product_id not in cart_data:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Product with id {product_id} not found in cart"
+            )
+        del cart_data[product_id]
+        return cart_data
