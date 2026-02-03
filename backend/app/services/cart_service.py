f31915b2f33692a1142675215cffdd349b8b4140
shalkovk <kshalkovskiy@gmail.com>
2026-02-03 23:49:40 +0500

    add cart service methods

--- backend/app/services/cart_service.py
+++ backend/app/services/cart_service.py
@@ -43,3 +43,29 @@ class CartSerice:
             )
         del cart_data[product_id]
         return cart_data
+
+    def get_cart_details(self, cart_data: Dict[int, int]) -> CartReponse:
+        if not cart_data:
+            return CartReponse(items=0, total=0.0, items_count=0)
+
+        product_ids = list(cart_data.keys())
+        products = self.product_repository.get_multiple_by_ids(product_ids)
+        products_dict = {product.id: product for product in products}
+
+        cart_items = []
+        total_price = 0.0
+        total_items = 0
+
+        for product_id, quantity in cart_data.items():
+            if product_id in products_dict:
+                product = products_dict[product_id]
+                subtotal = product.price * quantity
+
+            cart_item = CartItem(product_id=product_id, name=product.name, price=product.price,
+                                 quantity=quantity, subtotal=subtotal, image_url=product.image_url)
+
+            cart_items.append(cart_item)
+            total_price += subtotal
+            total_items += quantity
+
+        return CartReponse(items=cart_items, total=round(total_price), items_count=total_items)
